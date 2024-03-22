9e53522a8ecb3f42054c65f52a03fa89ca8eb5f2
Carla Antonini <carla.antonini@hotmail.com>
2024-03-22 03:13:04 -0300

    CA agrego PatchMatricula en interface

--- pkg/store/interface.go
+++ pkg/store/interface.go
@@ -3,14 +3,10 @@ package store
 import "github.com/Gaghyta/BackendIIIFinalGO/internal/domains"
 
 type StoreInterface interface {
-	// Read devuelve un odontologo por su id
 	Read(id int) (domains.Odontologo, error)
-	// Create agrega un nuevo odontologos
 	Create(odontologo domains.Odontologo) error
-	// Update actualiza un paciente
 	Update(odontologo domains.Odontologo) error
-	// Delete elimina un paciente
 	Delete(id int) error
-	// Exists verifica si un paciente existe
 	Exists(matricula string) bool
+	PatchMatricula(matricula string, nuevaMatricula string) error
 }
