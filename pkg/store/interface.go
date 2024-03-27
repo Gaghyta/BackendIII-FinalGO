8d01ca131ce467960cb9457bfce197845b3ec94c
Carla Antonini <carla.antonini@hotmail.com>
2024-03-27 17:51:40 -0300

    CA union de interface stroe

--- pkg/store/interface.go
+++ pkg/store/interface.go
@@ -5,15 +5,16 @@ import "github.com/Gaghyta/BackendIIIFinalGO/internal/domains"
 type StoreOdontologoInterface interface {
 	// Read devuelve un odontólogo por su id
 	Read(id int) (domains.Odontologo, error)
+	// GetByMatricula devuelve el id a partir de la matrícula
+	GetByMatricula(matricula string) (domains.Odontologo, error)
 	// Create agrega un nuevo odontólogo
-	Create(odontologo domains.Odontologo) error
+	Create(odontologo domains.Odontologo) (domains.Odontologo, error)
 	// Update actualiza un odontólogo
 	Update(id int, odontologo domains.Odontologo) (domains.Odontologo, error)
 	// Delete elimina un odontólogo
 	Delete(id int) error
 	// Exists verifica si un odontólogo existe
 	Exists(matricula string) bool
-	GetByMatricula(matricula string) (domains.Odontologo, error)
 }
 
 type StorePacienteInterface interface {
@@ -22,9 +23,9 @@ type StorePacienteInterface interface {
 	// ReadByDNI devuelve el id a partir del DNI
 	GetByDNI(dni string) (domains.Paciente, error)
 	// Create agrega un nuevo paciente
-	Create(paciente domains.Paciente) error
+	Create(paciente domains.Paciente) (domains.Paciente, error)
 	// Update actualiza un paciente
-	Update(paciente domains.Paciente) error
+	Update(id int, p domains.Paciente) (domains.Paciente, error)
 	// Delete elimina un paciente
 	Delete(id int) error
 	// Exists verifica si un paciente existe
@@ -37,9 +38,9 @@ type StoreTurnoInterface interface {
 	// GetByDNI devuelve un turno por su DNI
 	GetByDNI(dni string) (domains.Turno, error)
 	// Create agrega un nuevo turno
-	Create(turno domains.Turno) error
+	Create(turno domains.Turno) (domains.Turno, error)
 	// Update actualiza un turno
-	Update(turno domains.Turno) error
+	Update(turno domains.Turno) (domains.Turno, error)
 	// Delete elimina un turno
 	Delete(id int) error
 	// Exists verifica si un turno existe
