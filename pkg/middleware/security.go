2fe24ccad7438eac8a07ebfb59f64b54ec498ff8
Carla Antonini <carla.antonini@hotmail.com>
2024-03-27 03:50:08 -0300

    CA metodo authenticate en security

--- pkg/middleware/security.go
+++ pkg/middleware/security.go
@@ -1,28 +1,25 @@
 package middleware
 
-/* import (
-	"errors"
+import (
+	"net/http"
 	"os"
 
-	"github.com/BackendIIIFinalGO/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
-// Authentication manages the security by validating the token
-func Authentication() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		token := c.GetHeader("TOKEN")
-		if token == "" {
-			web.Failure(c, 401, errors.New("token not found"))
-			c.Abort()
+func Authenticate() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := os.Getenv("TOKEN")
+		tokenHeader := ctx.GetHeader("tokenPostman")
+
+		if tokenHeader != token {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"message": "Usuario invalido",
+			})
 			return
+		} else {
+			ctx.Next()
 		}
-		if token != os.Getenv("TOKEN") {
-			web.Failure(c, 401, errors.New("invalid token"))
-			c.Abort()
-			return
-		}
-		c.Next()
+
 	}
 }
-*/
